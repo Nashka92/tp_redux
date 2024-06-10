e480b4efda69914ab24fa5eb3874c6e3ff1bd720
Anna RAACH <annagraph92@gmail.com>
2024-06-10 13:43:15 +0200

    feat: add react modal

--- src/components/Product/ProductItem.jsx
+++ src/components/Product/ProductItem.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { deleteProduct, updateProduct } from "./productSlice";
+import Modal from "react-modal";
 
 const ProductItem = ({ product }) => {
   const dispatch = useDispatch();
@@ -24,31 +25,70 @@ const ProductItem = ({ product }) => {
     setIsEditing(false);
   };
 
+  // Pour fermer la modal
+  const closeModal = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div>
       <span>
         {product.nom} - {product.prix}€
       </span>
-      <button onClick={handleDelete} className="btn btn-danger btn-sm">Supprimer</button>
-      <button onClick={handleEdit} className="btn btn-info btn-sm">Modifier le produit</button>
+      <button onClick={handleDelete} className="btn btn-danger btn-sm">
+        Supprimer
+      </button>
+      <button onClick={handleEdit} className="btn btn-info btn-sm">
+        Modifier le produit
+      </button>
 
       {/* et là les  2 nouveaux champs d'affichent uniquement si on veut modifier le produit
         on lui rentre les nouvelles valeurs et on sauvegarde notre édition */}
-      {isEditing && (
-        <div>
+      <Modal
+        isOpen={isEditing}
+        onRequestClose={closeModal}
+        contentLabel="Modifier le produit"
+        ariaHideApp={false}
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            width: "300px",
+          },
+        }}
+      >
+        <h2>Modifier le produit</h2>
+        <form>
+          <label>Nom du nouveau produit</label>
           <input
+            className="border"
             type="text"
             value={newName}
             onChange={(e) => setNewName(e.target.value)}
           />
+          <label>Prix du nouveau produit</label>
           <input
+            className="border"
             type="number"
             value={newPrice}
             onChange={(e) => setNewPrice(parseFloat(e.target.value))}
           />
-          <button onClick={handleSave}>Enregistrer</button>
-        </div>
-      )}
+          <button
+            type="button"
+            onClick={handleSave}
+            className="btn btn-secondary"
+          >
+            Enregistrer
+          </button>
+          <button type="button" onClick={closeModal} className="btn btn-light">
+            Fermer
+          </button>
+        </form>
+      </Modal>
     </div>
   );
 };
